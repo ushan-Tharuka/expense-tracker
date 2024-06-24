c7887871da0d74b0ebfb4182d915139651100255
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-06-24 15:50:24 +0530

    first commit

---
+++ lib/pages/expences.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Expences',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.purple,
+        elevation: 0,
+        actions: [
+          Container(
+            color: Colors.yellow,
+            child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.black,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
